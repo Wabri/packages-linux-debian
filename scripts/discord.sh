b3dd3b02722c2cc2df1d01d787e2fb3f1329ec87
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-02-08 19:00:34 +0100

    Update (script): edit script
    * update translation of discord.sh
    * update download link of gitkraken.sh

--- scripts/discord.sh
+++ scripts/discord.sh
@@ -1,4 +1,4 @@
-# Discord chat vocale e testuale - https://discordapp.com/
+# Discord voice and text chat - https://discordapp.com/
 sudo apt install libgconf-2-4 libappindicator1
 wget -O discord.deb https://discordapp.com/api/download?platform=linux&format=deb
 sudo dpkg -i discord.deb
